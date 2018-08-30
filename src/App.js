03a4079b34b607f135240069eff073f1b6e4903e
JoyalAJohney <joyalajohny@gmail.com>
2018-08-30 20:10:30 +0530

    Errors fixed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import "./App.css";
 import { Provider } from "react-redux";
 import Store from "./redux/Store";
 import AddProduct from "./components/AddProduct";
-import Home from "./components/Home";
 import ProductDisplay from "./components/ProductDisplay";
 import SearchPage from "./components/searchPage";
 import Userpage from "./components/Userpage";
